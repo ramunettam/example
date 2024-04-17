8b0ae363aed491efe95b9f03ffbfda4502625312
ramunettam <ramunettam@gmail.com>
2024-04-17 09:49:17 +0200

    sum through cmd

---
+++ sum2.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+# Calculate the sum via command-line arguments
+# $1 and $2 refers to the first and second argument passed as command-line arguments
+
+# Calculate sum
+sum=$(( $1 + $2 ))
+
+# Display the result
+echo "Sum is: $sum"   
