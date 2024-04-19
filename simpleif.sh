dc4391b86ceeeb729400ccee2b6d18cf9a9c7c98
ramunettam <ramunettam@gmail.com>
2024-04-19 11:43:27 +0200

    shell example

--- simpleif.sh
+++ simpleif.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-n1=$1
-n2=$2
+number1=$1
+number2=$2
 
 if[$n1 -gt $n2]
 then
- echo "$n1 is bigger"
+ echo "$number1 is bigger"
  else
- echo "$n2 is biger"
+ echo "$number2 is biger"
  fi
